4b68a3a9f3bd86d265cd0d236d87bb4563e3c859
perico <pere@nubiola.cat>
2014-09-21 13:10:14 +0200

    addind new files

---
+++ EstherEdu/obj/local/x86/objs/estheredu/src/sslfunc.o.d
@@ -0,0 +1,226 @@
+obj/local/x86/objs/estheredu/src/sslfunc.o: jni/src/sslfunc.c \
+ jni/include/openssl/ssl.h jni/include/openssl/e_os2.h \
+ jni/include/openssl/opensslconf.h jni/include/openssl/bio.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdio.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/cdefs.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/android/api-level.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdint.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/_types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/_types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/posix_types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/stddef.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/compiler.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/posix_types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/kernel.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h \
+ jni/include/openssl/crypto.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdlib.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/string.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/malloc.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/alloca.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/strings.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/memory.h \
+ jni/include/openssl/stack.h jni/include/openssl/safestack.h \
+ jni/include/openssl/opensslv.h jni/include/openssl/ossl_typ.h \
+ jni/include/openssl/symhacks.h jni/include/openssl/x509.h \
+ jni/include/openssl/buffer.h jni/include/openssl/evp.h \
+ jni/include/openssl/objects.h jni/include/openssl/obj_mac.h \
+ jni/include/openssl/asn1.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/time.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/time.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/time.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/siginfo.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm-generic/siginfo.h \
+ jni/include/openssl/bn.h jni/include/openssl/ec.h \
+ jni/include/openssl/ecdsa.h jni/include/openssl/ecdh.h \
+ jni/include/openssl/rsa.h jni/include/openssl/dsa.h \
+ jni/include/openssl/dh.h jni/include/openssl/sha.h \
+ jni/include/openssl/x509_vfy.h jni/include/openssl/lhash.h \
+ jni/include/openssl/pkcs7.h jni/include/openssl/pem.h \
+ jni/include/openssl/pem2.h jni/include/openssl/hmac.h \
+ jni/include/openssl/kssl.h jni/include/openssl/ssl2.h \
+ jni/include/openssl/ssl3.h jni/include/openssl/tls1.h \
+ jni/include/openssl/dtls1.h jni/include/openssl/pqueue.h \
+ jni/include/openssl/ssl23.h jni/include/openssl/srtp.h \
+ jni/include/sslfunc.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/select.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/signal.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/limits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/limits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/limits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/internal_types.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/limits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/syslimits.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/page.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/page_32.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/signal.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm-generic/signal.h \
+ /usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/sigcontext.h
+
+jni/include/openssl/ssl.h:
+
+jni/include/openssl/e_os2.h:
+
+jni/include/openssl/opensslconf.h:
+
+jni/include/openssl/bio.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdio.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/cdefs.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/android/api-level.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdint.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/_types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/_types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/posix_types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/stddef.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/compiler.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/posix_types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/posix_types_32.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/kernel.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/sysmacros.h:
+
+jni/include/openssl/crypto.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/stdlib.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/string.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/malloc.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/alloca.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/strings.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/memory.h:
+
+jni/include/openssl/stack.h:
+
+jni/include/openssl/safestack.h:
+
+jni/include/openssl/opensslv.h:
+
+jni/include/openssl/ossl_typ.h:
+
+jni/include/openssl/symhacks.h:
+
+jni/include/openssl/x509.h:
+
+jni/include/openssl/buffer.h:
+
+jni/include/openssl/evp.h:
+
+jni/include/openssl/objects.h:
+
+jni/include/openssl/obj_mac.h:
+
+jni/include/openssl/asn1.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/time.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/time.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/time.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/siginfo.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm-generic/siginfo.h:
+
+jni/include/openssl/bn.h:
+
+jni/include/openssl/ec.h:
+
+jni/include/openssl/ecdsa.h:
+
+jni/include/openssl/ecdh.h:
+
+jni/include/openssl/rsa.h:
+
+jni/include/openssl/dsa.h:
+
+jni/include/openssl/dh.h:
+
+jni/include/openssl/sha.h:
+
+jni/include/openssl/x509_vfy.h:
+
+jni/include/openssl/lhash.h:
+
+jni/include/openssl/pkcs7.h:
+
+jni/include/openssl/pem.h:
+
+jni/include/openssl/pem2.h:
+
+jni/include/openssl/hmac.h:
+
+jni/include/openssl/kssl.h:
+
+jni/include/openssl/ssl2.h:
+
+jni/include/openssl/ssl3.h:
+
+jni/include/openssl/tls1.h:
+
+jni/include/openssl/dtls1.h:
+
+jni/include/openssl/pqueue.h:
+
+jni/include/openssl/ssl23.h:
+
+jni/include/openssl/srtp.h:
+
+jni/include/sslfunc.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/select.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/signal.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/limits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/limits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/linux/limits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/internal_types.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/machine/limits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/sys/syslimits.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/page.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/page_32.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/signal.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm-generic/signal.h:
+
+/usr/share/android/android-ndk-r10/platforms/android-19/arch-x86/usr/include/asm/sigcontext.h:
